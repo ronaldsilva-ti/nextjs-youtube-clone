dac4a34cc053debf73da7f5cc3605385919567e1
ronaldsilva <alvesronald@yahoo.com>
2021-03-29 08:37:42 -0300

    configured material ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default function Home() {
   return (
     <div>
       <h1>teste</h1>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
