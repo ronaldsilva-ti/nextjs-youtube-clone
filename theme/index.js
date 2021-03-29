e4afa5fe6e7ae060796bd16cde5ac9e1cdeaade1
ronaldsilva <alvesronald@yahoo.com>
2021-03-29 09:11:20 -0300

    configured theme

--- theme/index.js
+++ theme/index.js
@@ -1,20 +1,23 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { colors } from '@material-ui/core';
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
+    background: {
+      default: colors.common.white,
+      dark: '#f4f6f8',
+      paper: colors.common.white,
+    },
     primary: {
-      main: '#556cd6',
+      main: '#f44336',
     },
     secondary: {
-      main: '#19857b',
+      main: '#3EA6FF',
     },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
+    text: {
+      primary: colors.blueGrey[900],
+      secondary: colors.blueGrey[600],
     },
   },
 });
